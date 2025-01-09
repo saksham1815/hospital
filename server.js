ba466ea74870a830d0e0495a8f25f449a95c1219
sakshamsaxena <122423845+saksham1815@users.noreply.github.com>
2025-01-09 23:11:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,14 @@ import 'dotenv/config'
 import { connectDB } from "./config/connectDB.js";
 import hospitalRouter from "./routes/hospitalRoutes.js";
 
+const corsOptions={
+    origin:'http://localhost:5173',
+    credentials:true,
+}
 
 const app=express();
+app.use(express.json());
+app.use(cors(corsOptions))
 
 
 app.use(express.json());
